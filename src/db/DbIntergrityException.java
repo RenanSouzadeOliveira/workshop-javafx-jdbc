f2da4a9344bd264e794f68c20d7537ee83d9d3a9
Renan Souza de Oliveira <renansouzasccp@gmail.com>
2022-04-17 19:53:20 -0300

    Adding database acess

---
+++ src/db/DbIntergrityException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbIntergrityException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbIntergrityException(String msg) {
+		super(msg);
+	}
+
+}
